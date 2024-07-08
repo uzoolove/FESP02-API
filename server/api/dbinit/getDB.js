8b954251a05f33bea86d668fe352cce30a7bc302
Kilyong Jeong <uzoolove@gmail.com>
2024-07-08 14:52:35 +0900

    📝

--- server/api/dbinit/getDB.js
+++ server/api/dbinit/getDB.js
@@ -3,7 +3,7 @@ import { MongoClient } from 'mongodb';
 async function getDB(clientId){
   // const url = `mongodb://localhost`;
   // const url = `mongodb+srv://ins:ins12%24%24@openmarket.5ppv889.mongodb.net/?retryWrites=true&w=majority`;
-  const url = `mongodb://yongssam:yongssam12%21%21@db.fesp.shop/00-sample`;
+  const url = `mongodb://sample:sample11%21%21@db.fesp.shop/00-sample`;
 
   console.log(`DB 접속 시도: ${url}`, clientId);
   
