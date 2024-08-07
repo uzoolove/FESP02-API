baf52e8cd04a1a4ff5e27989153b007f0f114e9a
Kilyong Jeong <uzoolove@gmail.com>
2024-08-07 17:42:20 +0900

    🧪 운영서버 로그레벨 낮춤

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -4,7 +4,7 @@ if(process.env.NODE_ENV == 'production'){
     root: 'logs',
     maxLogFiles: 365,
     allLogsFileName: 'log',
-    level: 3,  // log(0), trace(1), debug(2), info(3), warn(4), error(5), fatal(6)
+    level: 0,  // log(0), trace(1), debug(2), info(3), warn(4), error(5), fatal(6)
     format: ['{{timestamp}} <{{title}}> {{file}}:{{line}}({{method}}()) {{message}}'],
     dateformat: 'yyyy-mm-dd HH:MM:ss',
     inspectOpt: {
