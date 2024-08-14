8b43eecea06e8e24997f05b341ed54f6d3a2e9d2
Kilyong Jeong <uzoolove@gmail.com>
2024-08-14 10:10:04 +0900

    🧪

--- server/api/dbinit/next-level/data.js
+++ server/api/dbinit/next-level/data.js
@@ -5,6 +5,7 @@ import moment from 'moment';
 function getDay(day = 0) {
   return moment().add(day, 'days').format('YYYY.MM.DD');
 }
+
 function getTime(day = 0, second = 0) {
   return moment().add(day, 'days').add(second, 'seconds').format('YYYY.MM.DD HH:mm:ss');
 }
@@ -44,25 +45,15 @@ export const initData = async (clientId, nextSeq) => {
       },
     ],
     // 상품
-    product: [
-      
-    ],
+    product: [],
     // 주문
-    order: [
-      
-    ],
+    order: [],
     // 후기
-    reply: [
-      
-    ],
+    reply: [],
     // 장바구니
-    cart: [
-      
-    ],
+    cart: [],
     // 즐겨찾기/북마크
-    bookmark: [
-      
-    ],
+    bookmark: [],
     // QnA, 공지사항, 게시판
     post: [
       {
@@ -83,7 +74,7 @@ export const initData = async (clientId, nextSeq) => {
             user: {
               _id: 2,
               name: '네오',
-              image: `/files/${clientId}/user-neo.webp`
+              image: `/files/${clientId}/user-neo.webp`,
             },
             content: '1등',
             like: 5,
@@ -95,13 +86,13 @@ export const initData = async (clientId, nextSeq) => {
             user_id: 3,
             user: {
               _id: 3,
-              name: '어피치'
+              name: '어피치',
             },
             content: '넵',
             like: 7,
             createdAt: getTime(-2, -60 * 60 * 10),
             updatedAt: getTime(-2, -60 * 60 * 1),
-          }
+          },
         ],
         createdAt: getTime(-3, -60 * 60 * 2),
         updatedAt: getTime(-3, -60 * 60 * 2),
@@ -109,53 +100,141 @@ export const initData = async (clientId, nextSeq) => {
       {
         _id: await nextSeq('post'),
         type: 'music',
-        title: '일일 DJ, GD 입니다',
-        views: 5,
+        title: '음악 신청 게시판 사용 방법입니다.',
+        content: '안녕하세요.\r\n오늘 일일 DJ를 하게 된 용디입니다.\r\n\r\n누구나 DJ 게시글에 글을 등록하면 DJ가 될 수 있습니다.\r\n신청곡은 해당 게시글에 댓글로 요청하면 되고 등록한 순서대로 자동 재생이됩니다.\r\n사연도 같이 등록하면 자동으로 사연을 읽어주고 신청곡이 재생됩니다.\r\n\r\n재생할 신청곡이 없으면 DJ가 선곡한 곡이 순서대로 재생되고 한 곡 재생이 완료되면 새로운 신청곡을 체크해서 신청곡이 재생됩니다.',
+        videoInfoList: [
+          {
+            _id: 1,
+            videoId: 'E0C-tN9QJ3Q',
+            extra: {
+              title: '박혜경 - Rain (2002年)',
+            },
+            user: {
+              _id: 3,
+              name: '',
+            },
+            content: '',
+          },
+          {
+            _id: 2,
+            videoId: 'iSwxR-eO0QM',
+            extra: {
+              title: '이럴거면(가사) 아이비',
+            },
+            user: {
+              _id: 3,
+              name: '',
+            },
+            content: '',
+          },
+          {
+            _id: 3,
+            videoId: 'wq4HlLqYyRY',
+            extra: {
+              title: '헤이즈 (Heize) -  비가 오는 날엔 (2021) / 가사',
+            },
+            user: {
+              _id: 3,
+              name: '',
+            },
+            content: '',
+          },
+          {
+            _id: 4,
+            videoId: 'atz_aZA3rf0',
+            extra: {
+              title: 'Ne-Yo - Because Of You (Official Music Video)',
+            },
+            user: {
+              _id: 3,
+              name: '',
+            },
+            content: '',
+          },
+          {
+            _id: 5,
+            videoId: 'CGxUd7kjnuA',
+            extra: {
+              title: '허각 - 나를 잊지 말아요 [최고의 사랑 OST] [가사/Lyrics]',
+            },
+            user: {
+              _id: 3,
+              name: '',
+            },
+            content: '',
+          },
+        ],
+        views: 318,
         user: {
-          _id: 2,
-          name: '네오',
-          image: `/files/${clientId}/user-neo.webp`
+          _id: 3,
+          name: '어피치',
+          image: '/files/00-next-level/user-apeach.webp',
         },
-        content: '좋은 노래 많이 신청해 주세요.',
+        createdAt: getTime(-3, -60 * 60 * 20),
+        updatedAt: getTime(-2, -60 * 60 * 10),
+        seller_id: null,
         replies: [
           {
             _id: 1,
-            user_id: 2,
+            videoId: '4TWR90KJl84',
+            content: '넥스트 레베루 프로젝트니까 next level 신청합니다.',
+            extra: {
+              title: "aespa 에스파 'Next Level' MV",
+              thumbnail: 'https://i.ytimg.com/vi/4TWR90KJl84/default.jpg'
+            },  
             user: {
               _id: 2,
               name: '네오',
               image: `/files/${clientId}/user-neo.webp`
             },
-            like: 5,
-            content: '삐딱하게 틀어주세요',
-            videoId: 'RKhsHGfrFmY',
-            extra: {
-              artist: 'GD',
-              title: 'G-DRAGON - 삐딱하게(CROOKED) M/V',
-            },            
             createdAt: getTime(-2, -60 * 60 * 20),
-            updatedAt: getTime(-2, -60 * 60 * 2),
+            updatedAt: getTime(-2, -60 * 60 * 2)
           },
           {
             _id: 2,
-            user_id: 3,
+            videoId: '',
+            content: '그냥 댓글만 남겨봅니다.',
             user: {
-              _id: 3,
-              name: '어피치'
-            },
-            content: '비가오니까 에픽하이의 우산이 듣고 싶어요.',
-            like: 13,
+              _id: 2,
+              name: '네오',
+              image: `/files/${clientId}/user-neo.webp`
+            },            
+            createdAt: getTime(-2, -60 * 60 * 20),
+            updatedAt: getTime(-2, -60 * 60 * 20)
+          },          
+          {
+            _id: 3,
             videoId: 'NIPtyAKxlRs',
+            content: '오늘처럼 비가 오는 날엔 이 노래죠',
             extra: {
-              artist: '에픽하이',
               title: '에픽하이(Epik high) - 우산 (Feat. 윤하)',
+              thumbnail: 'https://i.ytimg.com/vi/NIPtyAKxlRs/default.jpg'
             },
+            user: {
+              _id: 2,
+              name: '네오',
+              image: `/files/${clientId}/user-neo.webp`
+            },            
+            createdAt: getTime(-2, -60 * 60 * 13),
+            updatedAt: getTime(-2, -60 * 60 * 13)
+          },
+          {
+            _id: 4,
+            videoId: 'RKhsHGfrFmY',
+            content: '',
+            extra: {
+              title: 'G-DRAGON - 삐딱하게(CROOKED) M/V',
+              thumbnail: 'https://i.ytimg.com/vi/RKhsHGfrFmY/default.jpg'
+            },
+            user: {
+              _id: 2,
+              name: '네오',
+              image: `/files/${clientId}/user-neo.webp`
+            },            
             createdAt: getTime(-2, -60 * 60 * 10),
-            updatedAt: getTime(-2, -60 * 60 * 1),
+            updatedAt: getTime(-2, -60 * 60 * 10)
           }
-        ],
-        createdAt: getTime(-3, -60 * 60 * 2),
-        updatedAt: getTime(-3, -60 * 60 * 2),
+        ]
       }
     ],
     // 코드
